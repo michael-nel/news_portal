24814ff190ff1ca328138f2899c00b6ecc4e3c9f
Michael Nel <neckware@gmail.com>
2020-04-16 20:00:13 -0300

    Refactor Config & Routes

--- app.js
+++ app.js
@@ -1,21 +1,11 @@
-var express = require('express')
-var app = express();
+var app = require('./src/config/server');
 
-app.set('view engine', 'ejs');
+//Routes
+require('./src/routes/news')(app);
+require('./src/routes/home')(app);
+require('./src/routes/form_add')(app);
 
-app.get('/', (req, res) => {
-  res.render('./src/views/home/index');
-});
-
-app.get('/form_add', (req, res) => {
-  res.render('./src/views/admin/form_add_new');
-});
-
-app.get('/news', (req, res) => {
-  res.render('./src/views/news/news');
-});
-
-
+//Up Server
 app.listen(3000, () => {
   console.log('Server On ')
 });
---
+++ src/routes/form_add.js
@@ -0,0 +1,5 @@
+module.exports = (app) => {
+  app.get('/form_add', (req, res) => {
+    res.render('admin/form_add_new');
+  });
+}
